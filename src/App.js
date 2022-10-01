9af1b4a4d859e917fad61aba0be1df22fd9173bc
kukuhkuncoro <kukuhkuncoro92@gmail.com>
2022-10-01 17:32:11 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/navbar';
 import React from 'react';
+import Home from './components/home';
 
 function App() {
   return (
     <React.Fragment>
-      <div className="w-full h-screen bg-cover bg-center bg-[url('./assets/Mechanics.jpg')]">
+      <div className="inline-block align-middle w-full h-screen bg-cover bg-center bg-[url('./assets/Mechanics.jpg')]">
         <Navbar />
+        <Home />
       </div>
     </React.Fragment>
   );
